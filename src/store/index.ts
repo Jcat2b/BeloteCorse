21f32cadda8eb1d3bb3219e8aebe02b3d2af65e4
Jcat2b <104978388+Jcat2b@users.noreply.github.com>
2024-11-22 21:56:39 +0100

    Update src/store/index.ts [BoltSync]

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/authSlice';
+import gameReducer from './features/gameSlice';
+import socialReducer from './features/socialSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    game: gameReducer,
+    social: socialReducer,
   },
 });
 
