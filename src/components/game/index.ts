29233745d450d652230bfc78accb58aacdf106c3
Jcat2b <104978388+Jcat2b@users.noreply.github.com>
2024-11-23 20:21:14 +0100

    Update src/components/game/index.ts [BoltSync]

---
+++ src/components/game/index.ts
@@ -0,0 +1,11 @@
+export { default as GameBoard } from './GameBoard';
+export { default as GameInfo } from './GameInfo';
+export { default as PlayArea } from './PlayArea';
+export { default as PlayerHand } from './PlayerHand';
+export { default as BiddingPhase } from './BiddingPhase';
+export { default as TurnTimer } from './TurnTimer';
+export { default as DisconnectionAlert } from './DisconnectionAlert';
+export { default as WaitingPhase } from './WaitingPhase';
+export { default as GameTestControls } from './GameTestControls';
+export { default as AnnouncementDisplay } from './AnnouncementDisplay';
+export { default as PlayerPositions } from './PlayerPositions';
