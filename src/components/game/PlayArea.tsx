e3f22326c56eda77f564657d81f30a6725b1c3c7
Jcat2b <104978388+Jcat2b@users.noreply.github.com>
2024-11-24 14:11:31 +0100

    Update src/components/game/PlayArea.tsx [BoltSync]

--- src/components/game/PlayArea.tsx
+++ src/components/game/PlayArea.tsx
@@ -1,29 +1,39 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import type { RootState } from '../../store';
+import AnimatedCard from './AnimatedCard';
+import TrickHistory from './TrickHistory';
 
 const PlayArea: React.FC = () => {
   const { currentTrick, trumpSuit } = useSelector((state: RootState) => state.game);
+
+  const getCardPosition = (index: number) => {
+    switch (index) {
+      case 0: // Sud
+        return { x: 0, y: 100 };
+      case 1: // Est
+        return { x: 100, y: 0 };
+      case 2: // Nord
+        return { x: 0, y: -100 };
+      case 3: // Ouest
+        return { x: -100, y: 0 };
+      default:
+        return { x: 0, y: 0 };
+    }
+  };
 
   return (
     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <div className="relative w-96 h-96">
         {currentTrick.map((card, index) => (
-          <div
+          <AnimatedCard
             key={`${card.suit}-${card.value}`}
-            className={`absolute w-24 h-36 transform
-              ${index === 0 ? 'top-1/2 -translate-y-1/2' : ''}
-              ${index === 1 ? 'right-0 top-1/2 -translate-y-1/2' : ''}
-              ${index === 2 ? 'bottom-0 left-1/2 -translate-x-1/2' : ''}
-              ${index === 3 ? 'left-0 top-1/2 -translate-y-1/2' : ''}`}
-          >
-            <div className={`w-full h-full rounded-lg shadow-lg bg-white flex items-center justify-center
-              text-2xl font-bold ${card.suit === '♥' || card.suit === '♦' ? 'text-red-600' : 'text-black'}`}>
-              <span>{card.value}</span>
-              <span>{card.suit}</span>
-            </div>
-          </div>
+            card={card}
+            position={getCardPosition(index)}
+            rotation={index * 90}
+          />
         ))}
+        
         {trumpSuit && (
           <div className="absolute top-0 right-0 bg-white rounded-full p-2 shadow-lg">
             <span className={`text-2xl ${trumpSuit === '♥' || trumpSuit === '♦' ? 'text-red-600' : 'text-black'}`}>
@@ -32,6 +42,8 @@ const PlayArea: React.FC = () => {
           </div>
         )}
       </div>
+
+      <TrickHistory />
     </div>
   );
 };
