aada9cf13fee7e1bb7107e553f6af7ab99483e7e
Jcat2b <104978388+Jcat2b@users.noreply.github.com>
2024-11-24 14:11:33 +0100

    Update src/components/game/GameBoard.tsx [BoltSync]

--- src/components/game/GameBoard.tsx
+++ src/components/game/GameBoard.tsx
@@ -1,34 +1,34 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import type { RootState } from '../../store';
-import PlayerHand from './PlayerHand';
 import GameInfo from './GameInfo';
 import PlayArea from './PlayArea';
+import PlayerHand from './PlayerHand';
 import BiddingPhase from './BiddingPhase';
 import TurnTimer from './TurnTimer';
 import DisconnectionAlert from './DisconnectionAlert';
 import WaitingPhase from './WaitingPhase';
-import GameTestControls from './GameTestControls';
 import AnnouncementDisplay from './AnnouncementDisplay';
 import PlayerPositions from './PlayerPositions';
 import { useGameTimer } from '../../hooks/useGameTimer';
 import { useGameSync } from '../../hooks/useGameSync';
 import { useConnectionStatus } from '../../hooks/useConnectionStatus';
-import { useBotBehavior } from '../../hooks/useBotBehavior';
 
 const GameBoard: React.FC = () => {
   const { gameId } = useParams<{ gameId: string }>();
+  const navigate = useNavigate();
   const { phase } = useSelector((state: RootState) => state.game);
   const currentUser = useSelector((state: RootState) => state.auth.user);
-  const isTestMode = process.env.NODE_ENV === 'development';
 
   useGameTimer();
-  useGameSync(gameId!);
+  useGameSync(gameId || '');
   useConnectionStatus();
-  useBotBehavior();
 
-  if (!currentUser) return null;
+  if (!currentUser || !gameId) {
+    navigate('/');
+    return null;
+  }
 
   return (
     <div className="h-screen bg-green-800 p-4">
@@ -40,19 +40,19 @@ const GameBoard: React.FC = () => {
         <PlayerPositions />
         
         {phase === 'WAITING' && <WaitingPhase />}
+        
         {phase === 'BIDDING' && (
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <BiddingPhase />
           </div>
         )}
+        
         {phase === 'PLAYING' && (
           <>
             <PlayArea />
             <PlayerHand />
           </>
         )}
-
-        {isTestMode && <GameTestControls />}
       </div>
     </div>
   );
