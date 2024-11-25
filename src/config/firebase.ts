3c6da93972896f6890335fc8919a76bf9274463b
Jcat2b <104978388+Jcat2b@users.noreply.github.com>
2024-11-25 17:29:17 +0100

    Update src/config/firebase.ts [BoltSync]

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,16 +1,22 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyB0StH_qLbu8znWGzGW9kz3EpvzlQQVPcc",
+  authDomain: "belotecorse-44fae.firebaseapp.com",
+  projectId: "belotecorse-44fae",
+  storageBucket: "belotecorse-44fae.firebasestorage.app",
+  messagingSenderId: "222386478070",
+  appId: "1:222386478070:web:3141ccb4e51e55a93e85d7",
+  measurementId: "G-Q3J0077PVQ"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
